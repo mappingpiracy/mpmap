4bfa023dc03ff1014943a7e009d5d26d860d392c
Alex Klibisz <aklibisz@utk.edu>
2015-01-29 00:38:06 -0500

    some more cleanup

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,18 +10,9 @@ import views.html.index;
 
 import java.util.List;
 
-//import javax.inject.Inject;
-
 public class Application extends Controller {
 
     public static Result index() {
-       /* MybatisMapper mapper = new MybatisMapper();
-        SqlSession session = mapper.getSession();
-        EventMapper eventMapper = session.getMapper(EventMapper.class);
-
-        List<Country> countries = eventMapper.getCountries();
-         */
-
-        return ok(index.render());
+       return ok(index.render());
     }
 }
