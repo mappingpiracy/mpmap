37da041b225cb7554ed279aba29304e74b2c05ea
Alex Klibisz <aklibisz@utk.edu>
2015-02-07 23:40:39 -0500

    comment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#assumes the activator file is in the same directory
 sudo killall -u mpmap
 echo "***GENERATING NEW APPLICATION SECRET***"
 sudo -u mpmap ./activator play-generate-secret
