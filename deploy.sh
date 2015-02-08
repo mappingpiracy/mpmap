92c365371dd62f40254db17bd0eadbcd8fc29d84
Alex Klibisz <aklibisz@utk.edu>
2015-02-07 23:46:32 -0500

    added cd to mpmap dir

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 #assumes the activator file is in the same directory
 
 sudo killall -u mpmap
- 
+
+cd /var/www/mpmap 
 echo "***GENERATING NEW APPLICATION SECRET***"
 sudo -u mpmap ./activator play-generate-secret
 echo "***STARTING PLAY APPLICATION***"
