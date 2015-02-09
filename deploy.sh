318f99c1f727388af1aa710ef1ab12c9682a4254
Alex Klibisz <aklibisz@utk.edu>
2015-02-09 06:04:41 +0000

    live changes

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#assumes the activator file is in the same directory
+
+sudo killall -u mpmap
+
+cd /var/www/mpmap 
+#echo "***GENERATING NEW APPLICATION SECRET***"
+#sudo -u mpmap ./activator play-generate-secret > /dev/null
+echo "***STARTING PLAY APPLICATION***"
+sudo -u mpmap ./activator start
