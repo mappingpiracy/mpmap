1bc7b287c26bbfe7fb8dd8795517a954c108671a
Alex Klibisz <aklibisz@utk.edu>
2015-02-12 07:09:26 +0000

    added pull to deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,11 @@
 
 sudo killall -u mpmap
 
-cd /var/www/mpmap 
+cd /var/www/mpmap
 #echo "***GENERATING NEW APPLICATION SECRET***"
 #sudo -u mpmap activator play-generate-secret
+echo "***PULLING FROM MASTER***"
+sudo git pull origin master
+sudo chown -R mpmap:mpmap .
 echo "***STARTING PLAY APPLICATION***"
 sudo -u mpmap activator start
