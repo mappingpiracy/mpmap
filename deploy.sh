e32a663dd4efbcbb6ce989d26ceafa3a56a9a0a8
Alex Klibisz <aklibisz@utk.edu>
2015-02-24 19:06:47 -0500

    app secret

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 sudo killall -u mpmap
 
 cd /var/www/mpmap
-#echo "***GENERATING NEW APPLICATION SECRET***"
-#sudo -u mpmap activator play-generate-secret
 echo "***PULLING FROM MASTER***"
 sudo git pull origin master
 sudo chown -R mpmap:mpmap .
+echo "***GENERATING NEW APPLICATION SECRET***"
+sudo -u mpmap activator play-generate-secret
 echo "***STARTING PLAY APPLICATION***"
 sudo -u mpmap activator start
