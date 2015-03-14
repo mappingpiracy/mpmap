cfbb6ab9e68f6c225deb79c2874206d0e4bbfed5
Alex Klibisz <aklibisz@utk.edu>
2015-03-14 14:51:14 -0400

    added permissions to deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ cd /var/www/mpmap
 echo "***PULLING FROM MASTER***"
 sudo git pull origin master
 sudo chown -R mpmap:mpmap .
+sudo chmod -r 400 ./conf/*
 echo "***GENERATING NEW APPLICATION SECRET***"
 sudo -u mpmap activator play-generate-secret
 echo "***STARTING PLAY APPLICATION***"
